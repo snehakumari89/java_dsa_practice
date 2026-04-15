8753cbf75234af71349faf9cdab3c6942e695174
sneha kumari <snehavr1996@gmail.com>
2026-04-16 00:19:40 +0530

    first commit

---
+++ src/oops/encapsulation/Banking.java
@@ -0,0 +1,58 @@
+package oops.encapsulation;
+
+public class Banking {
+  private String accountHolderName;
+  private String accountNumber;
+  private double balance;
+
+  public Banking(String accountHolderName,String accountNumber,double  balance)
+  {
+      this.accountHolderName=accountHolderName;
+      this.accountNumber=accountNumber;
+      this.balance=balance;
+  }
+  //getter
+  public String getAccountHolderName()
+  {
+      return this.accountHolderName;
+  }
+  //setter
+    public void setAccountHolderName(String name)
+    {
+        if(name!=null && !name.isEmpty())
+        {
+            this.accountHolderName=name;
+        }
+        else {
+            System.out.println("Invalid Name");
+        }
+    }
+    //getter for account number
+      public String getAccountNumber()
+      {
+        return this.accountNumber;
+      }
+   //getter for balance
+      public double getBalance()
+     {
+        return this.balance;
+     }
+   // for balance
+
+    public void deposit(double amount)
+    {
+       if(amount>0)
+       {
+           balance=balance+amount;
+       }
+    }
+    public void withdraw(double amount)
+    {
+        if(amount>0 && amount<=balance)
+        {
+            balance=balance-amount;
+        }
+    }
+
+
+}
---
+++ src/oops/encapsulation/Main.java
@@ -0,0 +1,15 @@
+package oops.encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Banking acc=new Banking("Sneha","1234",5000);
+        System.out.println(acc.getAccountHolderName());
+        acc.deposit(20000);
+        System.out.println(acc.getBalance());
+        acc.withdraw(500);
+        System.out.println("final amount"+" "+acc.getBalance());
+        acc.setAccountHolderName("Guriya");
+        System.out.println(acc.getAccountHolderName());
+
+    }
+}
